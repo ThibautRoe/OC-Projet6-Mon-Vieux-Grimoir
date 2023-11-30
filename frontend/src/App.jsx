f178487315a16710e9418be151ba3a90fd8103f3
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2023-11-30 18:19:26 +0100

    Trying to get backend to work on Netlify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
-  BrowserRouter, Route, Routes,
+    BrowserRouter, Route, Routes,
 } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Home from './pages/Home/Home';
@@ -14,28 +14,28 @@ import { useUser } from './lib/customHooks';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const { connectedUser } = useUser();
+    const [user, setUser] = useState(null);
+    const { connectedUser } = useUser();
 
-  useEffect(() => {
-    setUser(connectedUser);
-  }, [connectedUser]);
-  return (
-    <BrowserRouter>
-      <div>
-        <ScrollToTop />
-        <Header user={user} setUser={setUser} />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
-          <Route path={APP_ROUTES.BOOK} element={<Book />} />
-          <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
-          <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    useEffect(() => {
+        setUser(connectedUser);
+    }, [connectedUser]);
+    return (
+        <BrowserRouter>
+            <div>
+                <ScrollToTop />
+                <Header user={user} setUser={setUser} />
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path={APP_ROUTES.SIGN_IN} element={<SignIn setUser={setUser} />} />
+                    <Route path={APP_ROUTES.BOOK} element={<Book />} />
+                    <Route path={APP_ROUTES.UPDATE_BOOK} element={<UpdateBook />} />
+                    <Route path={APP_ROUTES.ADD_BOOK} element={<AddBook />} />
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
