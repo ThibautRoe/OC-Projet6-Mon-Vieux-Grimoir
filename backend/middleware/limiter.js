2dd6197dbad8165e3a4b03ed2814a6a4e93ba68a
ThibautRoe <137530922+ThibautRoe@users.noreply.github.com>
2023-12-22 10:12:53 +0100

    Removed warning from express-rate-limit for Vercel + added console.log on front-end homepage for debug

--- backend/middleware/limiter.js
+++ backend/middleware/limiter.js
@@ -1,13 +1,41 @@
 import { rateLimit } from "express-rate-limit"
 
 // 100 requêtes max / 1sec
-export const globalLimiter = rateLimit({ limit: 100, windowMs: 1000 })
+export const globalLimiter = rateLimit({
+    limit: 100,
+    windowMs: 1000,
+    validate: {
+        xForwardedForHeader: false, // To avoid warning on Vercel
+        default: true,
+    },
+})
 
 // 10 requêtes max / 5sec
-export const authLimiter1 = rateLimit({ limit: 10, windowMs: 5 * 1000 })
+export const authLimiter1 = rateLimit({
+    limit: 10,
+    windowMs: 5 * 1000,
+    validate: {
+        xForwardedForHeader: false,
+        default: true,
+    },
+})
 
 // 50 requêtes max / 30min
-export const authLimiter2 = rateLimit({ limit: 50, windowMs: 30 * 60 * 1000 })
+export const authLimiter2 = rateLimit({
+    limit: 50,
+    windowMs: 30 * 60 * 1000,
+    validate: {
+        xForwardedForHeader: false,
+        default: true,
+    },
+})
 
 // 5000 requêtes max / 1min
-export const booksLimiter = rateLimit({ limit: 5000, windowMs: 60 * 1000 })
+export const booksLimiter = rateLimit({
+    limit: 5000,
+    windowMs: 60 * 1000,
+    validate: {
+        xForwardedForHeader: false,
+        default: true,
+    },
+})
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,27 @@
 /*
      eslint-disable react/jsx-filename-extension
 */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { API_ROUTES } from "./utils/constants"
+
+console.log(API_ROUTES.SIGN_UP)
+console.log(API_ROUTES.SIGN_IN)
+console.log(API_ROUTES.BOOKS)
+console.log(API_ROUTES.BEST_RATED)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
